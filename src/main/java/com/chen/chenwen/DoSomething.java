256b913e750465fce3a3708b14b09f69dccea06d
fanjic <404844679@qq.com>
2022-02-25 01:03:03 +0800

    又更新DoSomething

--- src/main/java/com/chen/chenwen/DoSomething.java
+++ src/main/java/com/chen/chenwen/DoSomething.java
@@ -1,7 +1,22 @@
 package com.chen.chenwen;
 
+import com.chen.chenwen.constant.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DoSomething {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        /*List<Integer> list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        for (Integer l : list) {
+
+        }*/
+
+        System.out.println(Color.YELLOW.getColor());
+        System.out.println(Color.getName(2));
     }
 }
